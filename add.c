458cb45ff683c3575e78c3865cdae1b4487e19a4
Matthew Favela <92203491+Chewy42@users.noreply.github.com>
2023-01-10 23:59:04 -1000

    initial commit
    simple addition

---
+++ add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  int num1, num2, sum;
+ scanf("Please enter two numbers: %d %d", num1, num2);
+  sum = num1 + num2;
+  printf("The sum is: %d\n");
+  return 0;
+}
