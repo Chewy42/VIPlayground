5fdca698a49ca821dc9820d5460ab0608a454f29
Matthew Favela <92203491+Chewy42@users.noreply.github.com>
2023-01-11 15:19:49 -1000

    Update add.c
    Working on linked list

--- add.c
+++ add.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char *argv[]){
   int num1, num2, sum;
  scanf("Please enter two numbers: %d %d", num1, num2);
   sum = num1 + num2;
   printf("The sum is: %d\n");
   return 0;
+}
+
+//linked list
+struct ListNode(int data){
+
 }
