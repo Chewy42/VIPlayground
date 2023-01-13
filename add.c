5650a8ea1947d54960538310cd2ad581e31939c5
Matthew Favela <92203491+Chewy42@users.noreply.github.com>
2023-01-12 23:22:46 -0800

    Update add.c

--- add.c
+++ add.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]){
-  int num1, num2, sum;
- scanf("Please enter two numbers: %d %d", num1, num2);
-  sum = num1 + num2;
-  printf("The sum is: %d\n");
+  //test out linked list
   return 0;
 }
 
